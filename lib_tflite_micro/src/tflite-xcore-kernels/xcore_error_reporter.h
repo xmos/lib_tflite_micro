113fb4ffc6cffa6259f2f531d35eb937f086cbf2
Henk Muller <henk@xmos.com>
2021-08-17 09:16:27 +0100

    Added error reporter that leaves a log of last N messages

---
+++ lib_tflite_micro/src/tflite-xcore-kernels/xcore_error_reporter.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2019, XMOS Ltd, All rights reserved
+
+#ifndef XCORE_ERROR_REPORTER_H_
+#define XCORE_ERROR_REPORTER_H_
+
+#include "tensorflow/lite/micro/compatibility.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+
+namespace tflite {
+namespace micro {
+namespace xcore {
+
+class XCoreErrorReporter : public tflite::MicroErrorReporter {
+ public:
+    explicit XCoreErrorReporter(){};
+    ~XCoreErrorReporter() override = default;
+    void Init(char *debugBuffer, int debugBufferLength);
+    void Log(const char* format, va_list args);
+    int Report(const char* format, va_list args) override;
+
+ private:
+    char* buffer;
+    int max_len;
+    int len = 0;
+    TF_LITE_REMOVE_VIRTUAL_DELETE
+};
+
+}  // namespace xcore
+}  // namespace micro
+}  // namespace tflite
+
+#endif  // XCORE_ERROR_REPORTER_H_

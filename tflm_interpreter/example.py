c5af4d96b0d2b9d6d303e13fc749fee9dbb33cae
Henk Muller <henk@xmos.com>
2021-11-02 11:34:31 +0000

    Fixeds to make load from flash work

--- tflm_interpreter/example.py
+++ tflm_interpreter/example.py
@@ -6,15 +6,13 @@ import numpy as np
 import cv2
 from tflm_interpreter import TFLMInterpreter
 
-ie = TFLMInterpreter(model_path = sys.argv[1])
+ie = TFLMInterpreter(model_path = sys.argv[1], params_path = sys.argv[2])
 
-img = cv2.imread(sys.argv[2])
-img = cv2.resize(img, (128,128))
-img = img[:, :, ::-1]  # Channel swapping due to mismatch between open CV and XMOS
-img = np.asarray(img, dtype=np.float32)
-img = img / 256.0
+with open(sys.argv[3], 'rb') as fd:
+    img = fd.read()
 
 ie.set_input_tensor(0, img)
 ie.invoke()
-answer = ie.get_output_tensor(0)
-print(answer)
+answer1 = ie.get_output_tensor(0)
+answer2 = ie.get_output_tensor(1)
+print(answer1, answer2)

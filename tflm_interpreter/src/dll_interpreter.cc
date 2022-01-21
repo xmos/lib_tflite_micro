cb6d9f14e11c9eb57209085d6cc4991410dbe974
alexandracraciun <alexandrac@xmos.com>
2022-01-21 12:02:20 +0000

    Update dll_interpreter.cc

--- tflm_interpreter/src/dll_interpreter.cc
+++ tflm_interpreter/src/dll_interpreter.cc
@@ -25,6 +25,7 @@ void add_lib_vision_ops(tflite::MicroMutableOpResolver<TFLM_OPERATORS> *resolver
     resolver->AddLogicalAnd();
     resolver->AddMul();
     resolver->AddPadV2();
+    resolver->AddReduceMax();
     resolver->AddResizeBilinear();
     resolver->AddResizeNearestNeighbor();
     resolver->AddRound();

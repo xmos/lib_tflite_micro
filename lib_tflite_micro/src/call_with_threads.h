235264ec12e131d568d5498a6d0873c39ce2e1c6
Henk Muller <henk@xmos.com>
2022-02-18 09:46:54 +0000

    Micro threading library that is completely in sync v1

---
+++ lib_tflite_micro/src/call_with_threads.h
@@ -0,0 +1,55 @@
+#if !defined(__micro_thread_library_h__)
+#define __micro_thread_library_h__
+
+#include <stdint.h>
+
+typedef struct {
+    uint32_t data[7];
+} thread_info_t;
+
+typedef void (*function_pointer)();
+/** Function that creates threads, then calls a function, then destroys threads
+ * This function creates four threads.
+ *
+ * \param   f      Function to be called when threads are created
+ * \param   ptr    Poitner to a thread_info block. The thread-ids will 
+ *                 be stored in this block, and a stack pointer is expected
+ *                 in this block:
+ *                     ptr[0] [out] thread-id-0    (versions with fewer threads
+ *                     ptr[1] [out] thread-id-1     will only use the first few
+ *                     ptr[2] [out] thread-id-2     slots)
+ *                     ptr[3] [out] thread-id-3
+ *                     ptr[4] [out] synchroniser-id
+ *                     ptr[5] [in]  top of stacks
+ *                     ptr[6] [in]  number of words per stack
+ */
+void call_with_threads_4_(function_pointer f, thread_info_t *ptr);
+
+/** Function that sets up one of the client threads
+ * This particular passes three arguments to the thread
+ * The thread function will be called with the three arguments.
+ * Note - we can make versions with more or fewer parameters.
+ * Note - we could pass this function the thread-function itself
+ *
+ * \param arg0      First argument for the thread function
+ * \param arg1      Second argument for the thread function
+ * \param arg2      Third argument for the thread function
+ * \param thread_id The thread_id to initialise; one of ptr[0]..ptr[3] above
+ */
+void thread_variable_setup(void *arg0, void *arg1, void *arg2, uint32_t thread_id);
+
+/** Function that starts all thread functions until completion.
+ * It is assumed that the variable parts have been set up per thread.
+ * by thread_variable_setup.
+ * This thread will also invoke the function with the given variable arguments.
+ *
+ * \param arg0      First argument for the master thread function
+ * \param arg1      Second argument for the master thread function
+ * \param arg2      Third argument for the master thread function
+ * \param fp        thread function to call on all threads.
+ * \param ptr       Pointer to the thread info block held in the xcore interpreter.
+ */
+void thread_function_call(void *arg0, void *arg1, void *arg2,
+                          function_pointer fp, thread_info_t *ptr);
+
+#endif

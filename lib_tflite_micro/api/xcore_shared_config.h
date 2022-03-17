aa30c4616da4c738d1f3145e878b6e79167a4e20
Deepak Panickal <deepakpanickal@xmos.com>
2022-03-17 16:23:48 +0000

    Add header for shared metadata

---
+++ lib_tflite_micro/api/xcore_shared_config.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2020, XMOS Ltd, All rights reserved
+#ifndef XCORE_SHARED_CONFIG_H_
+#define XCORE_SHARED_CONFIG_H_
+
+namespace shared_config {
+
+// This string is used as a key to store the shared config
+// between xformer and lib_tflite_micro in the flatbuffer
+constexpr char xcoreMetadataName[] = "xcoreSharedConfig";
+
+struct xcore_metadata {
+  int max_threads_used;
+};
+
+} // namespace shared_config
+
+#endif // XCORE_SHARED_CONFIG_H_

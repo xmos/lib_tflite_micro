1eb9d948fae28b621592996f45a426748f15d6f0
Aran <aranmcconnell@xmos.com>
2022-03-17 16:49:36 +0000

    parse custom options for memcpy strided_slice_v3

--- lib_tflite_micro/src/tflite-xcore-kernels/xcore_strided_slice_v3.cc
+++ lib_tflite_micro/src/tflite-xcore-kernels/xcore_strided_slice_v3.cc
@@ -24,80 +24,38 @@ namespace strided_slice_v3 {
 
 // This is the struct that contains the data required by the operator
 struct StridedSliceOpData : XCoreOpData {   // Inherits the operator name field from XCoreOpData
-    uint32_t width;
-    uint32_t height;
-    uint32_t channels;
-    uint32_t begin_x;
-    uint32_t begin_y;
-    uint32_t end_x;
-    uint32_t end_y;
-    uint32_t stride_x;
-    uint32_t stride_y;
+    nn::ImToColValid::Params *mf_params;
 };
+
+template <typename T>
+T* getDeserializedParams(TfLiteContext* context, const uint8_t* data) {
+  char* allocated_memory;
+  int allocationByteCount = sizeof(T);
+  allocated_memory =
+      (char*)context->AllocatePersistentBuffer(context, allocationByteCount);
+  T* param = T::template deserialise<T>(allocated_memory, (const char*)data);
+  return param;
+}
 
 void* Init(TfLiteContext* context, const char* buffer, size_t length) {
   auto op_data = construct_persistent_object<StridedSliceOpData>(context);
   op_data->name = "XC_Strided_Slice";
+  auto parser = CustomOptionParser(buffer, length);
+  const uint8_t *memcpy_fn_data = parser.parseNamedCustomOption("mp").AsBlob().data();
+  op_data->mf_params = getDeserializedParams<nn::ImToColValid::Params>(context, memcpy_fn_data);
   return op_data;
 }
 
 // Does all the requests for scratches
 TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
-  auto* op_data = static_cast<StridedSliceOpData*>(node->user_data);
-
-  //Get Inputs and set op data
-  const TfLiteTensor* input_ten = GetInput(context, node, 0);
-  const TfLiteTensor* begin_ten = GetInput(context, node, 1);
-  const TfLiteTensor* end_ten = GetInput(context, node, 2);
-  const TfLiteTensor* strides_ten = GetInput(context, node, 3);
-
-  op_data->width = SizeOfDimension(input_ten, 2);   
-  op_data->height = SizeOfDimension(input_ten, 1);
-  op_data->channels = SizeOfDimension(input_ten, 3);  
-
-  const uint32_t *begins = GetTensorData<uint32_t>(begin_ten);
-  op_data->begin_x = begins[2];
-  op_data->begin_y = begins[1];
-  if(!(op_data->begin_x < op_data->width)){
-    op_data->begin_x = op_data->width;
-  }
-  if(!(op_data->begin_y < op_data->height)){
-    op_data->begin_y = op_data->height;
-  }
-
-  const uint32_t *ends = GetTensorData<uint32_t>(end_ten);
-  op_data->end_x = ends[2];
-  op_data->end_y = ends[1];
-  if(!(op_data->end_x < op_data->width)){
-    op_data->end_x = op_data->width;
-  }
-  if(!(op_data->end_y < op_data->height)){
-    op_data->end_y = op_data->height;
-  }
-
-  const uint32_t *strides = GetTensorData<uint32_t>(strides_ten);
-  op_data->stride_x = strides[2];
-  op_data->stride_y = strides[1];
-  
   return kTfLiteOk;
 }
 
 TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   auto* op_data = static_cast<StridedSliceOpData*>(node->user_data);
-  
-  auto image_geom = nn::ImageGeometry(op_data->height, op_data->width, op_data->channels);
-  int x_diff = (int)op_data->end_x-(int)op_data->begin_x;
-  int y_diff = (int)op_data->end_y-(int)op_data->begin_y;
 
-  auto window_geom = nn::WindowGeometry({(y_diff/(int)op_data->stride_y)+(y_diff%(int)op_data->stride_y), (x_diff/(int)op_data->stride_x)+(x_diff%(int)op_data->stride_x), (int)op_data->channels},
-                                       {(int)op_data->begin_y, (int)op_data->begin_x},
-                                       {1, 1, 1},
-                                       {(int)op_data->stride_y, (int)op_data->stride_x});
+  auto memcpy = new (context->AllocatePersistentBuffer(context, sizeof(nn::ImToColValid::Params))) nn::ImToColValid(op_data->mf_params);
 
-  auto params = nn::ImToColValid::Params(image_geom, window_geom, op_data->channels);
-
-  auto memcpy = new (context->AllocatePersistentBuffer(context, sizeof(nn::ImToColValid))) nn::ImToColValid(&params);
-  
   //Get Input/Output Tensors
   const TfLiteEvalTensor *input = tflite::micro::GetEvalInput(context, node, 0);
   TfLiteEvalTensor *output = tflite::micro::GetEvalOutput(context, node, 0);
@@ -105,8 +63,8 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   //Pointers to data in In/Out Tensors
   void* in_data = const_cast<void *>(tflite::micro::GetTensorData<void>(input));
   void* out_data = tflite::micro::GetTensorData<void>(output);
-
-  memcpy->memcopy_fn((int8_t*)out_data, (int8_t*)in_data, op_data->begin_y, op_data->begin_x, 0);
+  
+  //memcpy->memcopy_fn((int8_t*)out_data, (int8_t*)in_data, op_data->mf_params->, op_data->begin_x, 0);
   
   return kTfLiteOk;
 }

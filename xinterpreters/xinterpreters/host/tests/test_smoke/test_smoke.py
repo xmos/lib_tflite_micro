89b047e7ce7fefeb35a952c2a8d598c9be92d3c6
aranXmos <93208072+aranXmos@users.noreply.github.com>
2022-05-10 14:04:23 +0100

    format wih black

--- xinterpreters/xinterpreters/host/tests/test_smoke/test_smoke.py
+++ xinterpreters/xinterpreters/host/tests/test_smoke/test_smoke.py
@@ -8,8 +8,8 @@ import cv2
 from xinterpreters import xcore_tflm_host_interpreter
 
 ie = xcore_tflm_host_interpreter()
-ie.set_model(model_path = "./smoke_model.tflite", params_path = "./smoke_model.flash")
-with open("./detection_0.raw", 'rb') as fd:
+ie.set_model(model_path="./smoke_model.tflite", params_path="./smoke_model.flash")
+with open("./detection_0.raw", "rb") as fd:
     img = fd.read()
 
 ie.set_input_tensor(0, img)
@@ -17,7 +17,7 @@ ie.invoke()
 
 answer1 = ie.get_output_tensor(0)
 answer2 = ie.get_output_tensor(1)
-with open("./out0", 'wb') as fd:
+with open("./out0", "wb") as fd:
     fd.write(answer1)
-with open("./out1", 'wb') as fd:
+with open("./out1", "wb") as fd:
     fd.write(answer2)

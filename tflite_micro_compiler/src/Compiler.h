0f47e241c06d3848d94d8a81cb65bc29b035c29c
Deepak Panickal <deepakpanickal@xmos.com>
2022-06-23 12:02:05 +0100

    Refactor xcore custom op handling

--- tflite_micro_compiler/src/Compiler.h
+++ tflite_micro_compiler/src/Compiler.h
@@ -8,6 +8,7 @@
 #include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/schema/schema_generated.h"
+#include "xcore_ops.h"
 
 namespace tflmc {
 

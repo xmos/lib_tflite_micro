5e5310a1b9b4bf2036c47a8aacd95cc0132db2bd
aranXmos <93208072+aranXmos@users.noreply.github.com>
2022-06-23 16:09:44 +0100

    3 ops to allow concat replacement

---
+++ lib_tflite_micro/src/tflite-xcore-kernels/xcore_connector.cc
@@ -0,0 +1,35 @@
+// Copyright (c) 2022, XMOS Ltd, All rights reserved
+
+#include "MemCpyFn.hpp"
+#include "xcore_custom_options.h"
+
+namespace tflite {
+namespace ops {
+namespace micro {
+namespace xcore {
+namespace connector {
+
+void *Init(TfLiteContext *context, const char *buffer, size_t length) {
+}
+
+// Does all the requests for scratches
+TfLiteStatus Prepare(TfLiteContext *context, TfLiteNode *node) {
+  return kTfLiteOk;
+}
+
+TfLiteStatus Eval(TfLiteContext *context, TfLiteNode *node) {
+  return kTfLiteOk;
+}
+
+} // namespace connector
+
+TfLiteRegistration *Register_XC_connector() {
+  static TfLiteRegistration r = {connector::Init, nullptr,
+                                 connector::Prepare, connector::Eval};
+  return &r;
+}
+
+} // namespace xcore
+} // namespace micro
+} // namespace ops
+} // namespace tflite
--- lib_tflite_micro/src/tflite-xcore-kernels/xcore_pass_thru.cc
+++ lib_tflite_micro/src/tflite-xcore-kernels/xcore_pass_thru.cc
@@ -7,21 +7,9 @@ namespace tflite {
 namespace ops {
 namespace micro {
 namespace xcore {
-namespace copy_into {
-
-// This is the struct that contains the data required by the operator
-struct CopyIntoOpData
-    : XCoreOpData { // Inherits the operator name field from XCoreOpData
-  int32_t offset;
-};
+namespace pass_thru {
 
 void *Init(TfLiteContext *context, const char *buffer, size_t length) {
-  auto op_data = construct_persistent_object<CopyIntoOpData>(context);
-  op_data->name = "XC_Copy_Into";
-
-  auto parser = CustomOptionParser(buffer, length);
-  op_data->offset = parser.parseNamedCustomOption("offset").AsInt32();
-  return op_data;
 }
 
 // Does all the requests for scratches
@@ -30,29 +18,26 @@ TfLiteStatus Prepare(TfLiteContext *context, TfLiteNode *node) {
 }
 
 TfLiteStatus Eval(TfLiteContext *context, TfLiteNode *node) {
-
-  auto *op_data = static_cast<CopyIntoOpData *>(node->user_data);
   // Get Input/Output Tensors
   const TfLiteEvalTensor *input = tflite::micro::GetEvalInput(context, node, 0);
-  const TfLiteEvalTensor *concatTensor = tflite::micro::GetEvalInput(context, node, 1);
+  const TfLiteEvalTensor *output = tflite::micro::GetEvalOutput(context, node, 0);
   // Pointers to data in In/Out Tensors
   void *in_data = (void *)(tflite::micro::GetTensorData<void>(input));
-  void *out_data = (void *)tflite::micro::GetTensorData<void>(concatTensor);
+  void *out_data = (void *)tflite::micro::GetTensorData<void>(output);
   // Get size of input tensor
   TfLiteIntArray* input_dims = input->dims;
   int32_t input_size = 1;
   for(int i = 0; i < input_dims->size; i++) input_size*input_dims->data[i];
   // Memcpy input to concatTensor plus offset
-  memcpy((out_data + op_data->offset), in_data, input_size);
-
+  memcpy(out_data, in_data, input_size);
   return kTfLiteOk;
 }
 
-} // namespace copy_into
+} // namespace pass_thru
 
-TfLiteRegistration *Register_XC_copy_into() {
-  static TfLiteRegistration r = {copy_into::Init, nullptr,
-                                 copy_into::Prepare, copy_into::Eval};
+TfLiteRegistration *Register_XC_pass_thru() {
+  static TfLiteRegistration r = {pass_thru::Init, nullptr,
+                                 pass_thru::Prepare, pass_thru::Eval};
   return &r;
 }
 

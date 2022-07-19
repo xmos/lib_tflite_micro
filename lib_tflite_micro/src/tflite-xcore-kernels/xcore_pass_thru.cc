36d5e331513e851d8ae26e812e89e062515a59ba
scott-xmos <scottroset@xmos.com>
2022-07-18 22:01:09 -0600

    copy_into, pass_thru alt implementation

--- lib_tflite_micro/src/tflite-xcore-kernels/xcore_pass_thru.cc
+++ lib_tflite_micro/src/tflite-xcore-kernels/xcore_pass_thru.cc
@@ -3,6 +3,7 @@
 #include "MemCpyFn.hpp"
 #include "xcore_custom_options.h"
 #include "xcore_utils.h"
+#include "tensorflow/lite/kernels/kernel_util.h"
 
 namespace tflite {
 namespace ops {
@@ -19,18 +20,21 @@ TfLiteStatus Prepare(TfLiteContext *context, TfLiteNode *node) {
 }
 
 TfLiteStatus Eval(TfLiteContext *context, TfLiteNode *node) {
-  // Get Input/Output Tensors
-  const TfLiteEvalTensor *input = tflite::micro::GetEvalInput(context, node, 0);
-  const TfLiteEvalTensor *output = tflite::micro::GetEvalOutput(context, node, 0);
-  // Pointers to data in In/Out Tensors
-  void *in_data = (void *)(tflite::micro::GetTensorData<void>(input));
-  void *out_data = (void *)tflite::micro::GetTensorData<void>(output);
-  // Get size of input tensor
-  TfLiteIntArray* input_dims = input->dims;
-  int32_t input_size = 1;
-  for(int i = 0; i < input_dims->size; i++) input_size*input_dims->data[i];
-  // Memcpy input to concatTensor plus offset
-  memcpy(out_data, in_data, input_size);
+  // // Get Input/Output Tensors
+  // const TfLiteEvalTensor *input = tflite::micro::GetEvalInput(context, node, 0);
+  // const TfLiteEvalTensor *output = tflite::micro::GetEvalOutput(context, node, 0);
+  // // Pointers to data in In/Out Tensors
+  // void *in_data = (void *)(tflite::micro::GetTensorData<void>(input));
+  // void *out_data = (void *)tflite::micro::GetTensorData<void>(output);
+  // // Get size of input tensor
+  // TfLiteIntArray* input_dims = input->dims;
+  // int32_t input_size = 1;
+  // for(int i = 0; i < input_dims->size; i++) input_size*input_dims->data[i];
+  // // Memcpy input to concatTensor plus offset
+  // memcpy(out_data, in_data, input_size);
+  const TfLiteTensor* input = GetInput(context, node, 0);
+  const TfLiteTensor* output = GetOutput(context, node, 0);
+  memcpy(output->data.raw, input->data.raw, input->bytes);
   return kTfLiteOk;
 }
 

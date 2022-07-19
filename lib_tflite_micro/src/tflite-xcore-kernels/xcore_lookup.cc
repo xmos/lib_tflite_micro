9f398cc4cc1838be62e5bff77cae8fbd556379a8
aranXmos <93208072+aranXmos@users.noreply.github.com>
2022-07-19 11:05:14 +0100

    removed branch in lookup

--- lib_tflite_micro/src/tflite-xcore-kernels/xcore_lookup.cc
+++ lib_tflite_micro/src/tflite-xcore-kernels/xcore_lookup.cc
@@ -15,7 +15,6 @@ struct LookupOpData
     : XCoreOpData { // Inherits the operator name field from XCoreOpData
 };
 
-
 void *Init(TfLiteContext *context, const char *buffer, size_t length) {
   auto op_data = construct_persistent_object<LookupOpData>(context);
   op_data->name = "XC_lookup";
@@ -31,34 +30,33 @@ TfLiteStatus Prepare(TfLiteContext *context, TfLiteNode *node) {
 TfLiteStatus Eval(TfLiteContext *context, TfLiteNode *node) {
 
   auto *op_data = static_cast<LookupOpData *>(node->user_data);
-  
+
   // Get Input/Output Tensors
   const TfLiteEvalTensor *input = tflite::micro::GetEvalInput(context, node, 0);
   int input_size = 1;
-  for(int i = 0; i < input->dims->size; i++) input_size *= input->dims->data[i];
+  for (int i = 0; i < input->dims->size; i++)
+    input_size *= input->dims->data[i];
   const TfLiteEvalTensor *table = tflite::micro::GetEvalInput(context, node, 1);
   TfLiteEvalTensor *output = tflite::micro::GetEvalOutput(context, node, 0);
 
   // Pointers to data in In/Out Tensors
-  //void *in_data = const_cast<void *>(tflite::micro::GetTensorData<void>(input));
-  void *table_vals = const_cast<void *>(tflite::micro::GetTensorData<void>(table));
+  void *table_vals =
+      const_cast<void *>(tflite::micro::GetTensorData<void>(table));
   void *out_data = tflite::micro::GetTensorData<void>(output);
   const int8_t *in_val = tflite::micro::GetTensorData<int8_t>(input);
-  
-  for(int i = 0; i < input_size; i++)
-  {
-    if(in_val[i] >= 0) memcpy((int8_t*)out_data + i, (int8_t*)table_vals + *(in_val + i), 1);
-    else memcpy((int8_t*)out_data + i, (int8_t*)table_vals + 256 + *(in_val + i), 1);
+
+  for (int i = 0; i < input_size; i++) {
+    ((int8_t *)out_data)[i] = ((int8_t *)table_vals)[((uint8_t *)in_val)[i]];
   }
-  
+
   return kTfLiteOk;
 }
 
 } // namespace lookup
 
 TfLiteRegistration *Register_XC_lookup() {
-  static TfLiteRegistration r = {lookup::Init, nullptr,
-                                 lookup::Prepare, lookup::Eval};
+  static TfLiteRegistration r = {lookup::Init, nullptr, lookup::Prepare,
+                                 lookup::Eval};
   return &r;
 }
 

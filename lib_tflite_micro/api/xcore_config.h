8687c5bbdd70b3d929e7836ad9447a9cd70d8d57
aranXmos <93208072+aranXmos@users.noreply.github.com>
2022-11-03 15:49:11 +0000

    fix allignment for xcore_config in xc includes

--- lib_tflite_micro/api/xcore_config.h
+++ lib_tflite_micro/api/xcore_config.h
@@ -10,7 +10,12 @@ struct xc_context_config_t {
 };
 #else
 struct xc_context_config_t {
+  uint64_t id_aligned[2]; // Guarantee 64-bit alignment.        // Actual IDs           // ids of at most 4 threads - live during invoke
+  uint32_t synchroniser;    // synchroniser for threads - live during invoke
+  uint32_t nstackwords;     // nstackwords per stack   - live after load model
+  void * UNSAFE stacks;             // pointer to top of stack - live after load model
   void * UNSAFE flash_data; // channel to flash reader.
+
 };
 #endif
 

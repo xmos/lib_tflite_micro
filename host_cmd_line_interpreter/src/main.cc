48dfa6df5ee5f53d255876380b6ef924149c47c4
panickal-xmos <deepakpanickal@xmos.com>
2023-07-18 10:44:25 +0100

    Update tests

--- host_cmd_line_interpreter/src/main.cc
+++ host_cmd_line_interpreter/src/main.cc
@@ -1,6 +1,6 @@
 // Copyright 2021 XMOS LIMITED. This Software is subject to the terms of the
 // XMOS Public License: Version 1
-#include "lib_tflite_micro/api/inference_engine.h"
+#include "inference_engine.h"
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>

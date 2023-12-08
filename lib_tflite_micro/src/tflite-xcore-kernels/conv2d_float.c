c63b3438eee83f69131a7d93e33f92d3c53bf00f
Henk Muller <henk@xmos.com>
2023-12-08 08:22:46 +0000

    Added FC packed float operation

--- lib_tflite_micro/src/tflite-xcore-kernels/conv2d_float.c
+++ lib_tflite_micro/src/tflite-xcore-kernels/conv2d_float.c
@@ -1,10 +1,13 @@
 #include "conv2d_float.h"
 #include <assert.h>
+#include <stdint.h>
+#include <string.h>
 
 int xc_fc_float_ref(float *outputs, float *inputs, float *kernels,
-                    int out_features, int input_features) {
+                    int out_features, int input_features, int out_f_start,
+                    int out_f_end) {
   int cnt = 0;
-  for (int f = 0; f < out_features; f++) {
+  for (int f = out_f_start; f < out_f_end; f++) {
     int output_index = f;
     float acc = 0;
     for (int kf = 0; kf < input_features; kf++) {
@@ -357,5 +360,152 @@ void xc_transpose_conv2d_float_kw5xh2_stride_h3_opt(
       }
     }
   }
+}
+#endif
+
+float extract3_ref(float *kernels, int index) {
+    float x;
+    memcpy(&x, ((uint8_t *)kernels) + index*3-1, 4);
+    return x;
+}
+
+#define extract3(fout, kernels, index) \
+    { \
+    switch(index & 3) { \
+    default: \
+        f0 = kernels[(index>>2)*3];                 \
+        asm volatile("lextract %0, %1, %2, %3, 32" : "=r" (fout) : "r" (f0), "r" (f0), "r" (24)); \
+        break; \
+    case 1: \
+        f1 = kernels[(index>>2)*3+1]; \
+        asm volatile("lextract %0, %1, %2, %3, 32" : "=r" (fout) : "r" (f1), "r" (f0), "r" (16)); \
+        break; \
+    case 2: \
+        f2 = kernels[(index>>2)*3+2]; \
+        asm volatile("lextract %0, %1, %2, %3, 32" : "=r" (fout) : "r" (f2), "r" (f1), "r" (8)); \
+        break; \
+    case 3: \
+        fout = f2; \
+        break; \
+    } \
+}
+
+int xc_fc_float_packed_ref(float *outputs, float *inputs, float *kernels,
+                    int out_features, int input_features, int out_f_start,
+                    int out_f_end) {
+  int cnt = 0;
+  for (int f = out_f_start ; f < out_f_end; f++) {
+    int output_index = f;
+    float acc = 0;
+    for (int kf = 0; kf < input_features; kf++) {
+      int input_index = kf;
+      int kernel_index = f * input_features + kf;
+      acc += inputs[input_index] * extract3_ref(kernels ,kernel_index);
+      cnt++;
+    }
+    outputs[output_index] = acc;
+  }
+  return cnt;
+}
+
+#ifndef NN_USE_REF
+int xc_fc_float_packed_opt(float *outputs, float *inputs, float *kernels,
+                    int out_features, int input_features, int out_f_start,
+                    int out_f_end) {
+  int cnt = 0;
+  float f0, f1, f2;
+  for (int f = out_f_start; f < out_f_end; f++) {
+    int output_index = f;
+    float acc = 0;
+    assert (input_features == 96);
+#pragma clang loop unroll_count(8)
+    for (int kf = 0; kf < input_features; kf++) {
+      int input_index = kf;
+      int kernel_index = f * input_features + kf;
+      float in1 = inputs[input_index];
+      float in2 = 0; extract3(in2, kernels ,kernel_index);
+      asm volatile("fmacc %0, %1, %2, %3"
+                   : "=r"(acc)
+                   : "r"(acc), "r"(in1), "r"(in2));
+      cnt++;
+    }
+    outputs[output_index] = acc;
+  }
+  return cnt;
+}
+#endif
+
+#ifdef LOCAL_MAIN
+
+static void pack_float(float *kernels, float *kernels_in, int num) {
+    for(int i = 0; i < num; i++) {
+        memcpy(((uint8_t *)kernels) + i*3, ((uint8_t * )&kernels_in[i])+1, 3);
+    }
+}
+
+#include <stdio.h>
+#include <math.h>
+
+int test_fc(int opt) {
+    float outputs[4];
+    float expected_outputs[4];
+    float inputs[96];
+    float kernels2[96*3];
+    float kernels[96*4];
+    for(int i=0; i<96; i++) {
+        inputs[i] = i*i;
+    }
+    for(int i=0; i<4*96; i++) {
+        kernels[i] = i;
+    }
+    for(int o = 0; o < 4; o++) {
+        float e = 0;
+        for(int i=0; i<96; i++) {
+            e += i*i * (i+o*96);
+        }
+        expected_outputs[o] = e;
+    }
+    int t0, t1;
+    pack_float(kernels2, kernels, 96*4);
+    asm volatile("gettime %0" : "=r" (t0));
+    switch(opt) {
+    case 0:
+        xc_fc_float_ref(outputs, inputs, kernels, 10, 96, 0, 4);
+        break;
+    case 1:
+        xc_fc_float_opt(outputs, inputs, kernels, 10, 96, 0, 4);
+        break;
+    case 2:
+        xc_fc_float_packed_ref(outputs, inputs, kernels2, 10, 96, 0, 4);
+        break;
+    case 3:
+        xc_fc_float_packed_opt(outputs, inputs, kernels2, 10, 96, 0, 4);
+        break;
+    }
+    asm volatile("gettime %0" : "=r" (t1));
+    printf("%d ticks\n", t1-t0);
+    int errors = 0;
+    for(int o = 0; o < 4; o++) {
+        if (fabs((expected_outputs[o]-outputs[o]) / expected_outputs[o]) > 1e-5 ) {
+            printf("Expected idx %d %f got %f func %d\n", o, expected_outputs[o], outputs[o], opt);
+            errors++;
+        }
+    }
+    return errors;
+
+}
+
+int main(void) {
+    int errors = 0;
+    errors += test_fc(0);
+    errors += test_fc(1);
+    errors += test_fc(2);
+    errors += test_fc(3);
+    if (errors) {
+        printf("FAIL\n");
+    } else {
+        printf("PASS\n");
+    }
+    return 0;
 }
 #endif

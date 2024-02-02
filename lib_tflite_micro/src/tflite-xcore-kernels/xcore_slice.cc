564d2e845b862b625e388e401e85db436a5f328b
Michael Poluektov <michael.poluektov@gmail.com>
2024-02-02 12:08:10 +0000

    change xcore_slice.cc

--- lib_tflite_micro/src/tflite-xcore-kernels/xcore_slice.cc
+++ lib_tflite_micro/src/tflite-xcore-kernels/xcore_slice.cc
@@ -1,7 +1,6 @@
 // Copyright (c) 2023, XMOS Ltd, All rights reserved
 
-#include "lib_nn/api/AbstractKernel.hpp"
-#include "lib_nn/api/MemCpyFn.hpp"
+#include "nn_op_utils.h"
 #include "xcore_custom_options.h"
 #include "xcore_utils.h"
 
@@ -11,46 +10,30 @@ namespace micro {
 namespace xcore {
 namespace slice {
 
-enum MemcpyType {
-  SliceCpy_t,
-  VpuCpy_t,
-  MemCpy_t,
-};
-
 // This is the struct that contains the data required by the operator
 struct SliceOpData
     : XCoreOpData { // Inherits the operator name field from XCoreOpData
-  int32_t begin_x;
-  int32_t begin_y;
-  nn::memcpyfn_imtocol_valid_params_t *mf_params;
-  nn::MemFnType memcpy_fn;
-  int32_t memcpy_type;
+  int32_t begin[5];
+  int32_t end[5];
+  int32_t in_offsets[4];
+  int32_t out_offsets[4];
+  bool is_vpu;
 };
 
-template <typename T>
-T *getDeserializedParams(TfLiteContext *context, const uint8_t *data) {
-
-  assert(((uintptr_t)data & 0x3) == 0);
-  T *param = (T *)data;
-  return param;
+void copy_blob(int32_t *dst, flexbuffers::Reference ref) {
+  auto blob = ref.AsBlob();
+  memcpy(dst, blob.data(), blob.size());
 }
 
 void *Init(TfLiteContext *context, const char *buffer, size_t length) {
   auto op_data = construct_persistent_object<SliceOpData>(context);
   op_data->name = "XC_Slice";
-
   auto parser = CustomOptionParser(buffer, length);
-
-  assert(((uintptr_t)parser.parseNamedCustomOption("mp").AsBlob().data() &
-          0x3) == 0);
-  op_data->mf_params =
-      (nn::memcpyfn_imtocol_valid_params_t *)parser.parseNamedCustomOption("mp")
-          .AsBlob()
-          .data();
-  op_data->begin_x = parser.parseNamedCustomOption("begin_x").AsInt32();
-  op_data->begin_y = parser.parseNamedCustomOption("begin_y").AsInt32();
-  op_data->memcpy_fn = (nn::MemFnType)nn::memcpyfn_imtocol_valid;
-  op_data->memcpy_type = parser.parseNamedCustomOption("type").AsInt32();
+  copy_blob(op_data->begin, parser.parseNamedCustomOption("b"));
+  copy_blob(op_data->end, parser.parseNamedCustomOption("e"));
+  copy_blob(op_data->in_offsets, parser.parseNamedCustomOption("i"));
+  copy_blob(op_data->out_offsets, parser.parseNamedCustomOption("o"));
+  op_data->is_vpu = parser.parseNamedCustomOption("v").AsBool();
   return op_data;
 }
 
@@ -59,6 +42,24 @@ TfLiteStatus Prepare(TfLiteContext *context, TfLiteNode *node) {
   return kTfLiteOk;
 }
 
+// [michael p] TFLite supports up to 5D tensors, last loop is replaced with
+// memcpy. If there are less than 5 dimensions: the first 5 - D elements of
+// begin/end are 0 and 1 respectively. To ensure optimal performance, the
+// compiler will re-write begin/end/offsets to have the sliced dimension fused
+// with the dimensions after it. Example: input_size = [10, 20, 5], begin = [1,
+// 0, 0], end = [8, 20, 5] will give begin = [0, 0, 0, 0, 1*20*5], end = [1, 1,
+// 1, 1, 8*20*5] as if shape was [1, 1, 1, 1, 10*20*5], allowing us to use
+// vpu_memcpy since the last dimension is a multiple of 4.
+//
+// If slice becomes a bottleneck:
+//
+// - For less memory usage, you can get rid of a few for loops, the operator
+// supports slicing along the number of for loops axes. 5 for loops are only
+// necessary if you slice along each axis of a batched video input.
+//
+// - For better performance without the last dimension being a multiple of 4,
+// you can check if vpu_memcpy_vec can be used on some iterations (up to 2x
+// improvement)
 TfLiteStatus Eval(TfLiteContext *context, TfLiteNode *node) {
 
   auto *op_data = static_cast<SliceOpData *>(node->user_data);
@@ -66,31 +67,36 @@ TfLiteStatus Eval(TfLiteContext *context, TfLiteNode *node) {
   const TfLiteEvalTensor *input = tflite::micro::GetEvalInput(context, node, 0);
   TfLiteEvalTensor *output = tflite::micro::GetEvalOutput(context, node, 0);
   // Pointers to data in In/Out Tensors
-  void *in_data = const_cast<void *>(tflite::micro::GetTensorData<void>(input));
+  const void *in_data = tflite::micro::GetTensorData<void>(input);
   void *out_data = tflite::micro::GetTensorData<void>(output);
-
-  if (op_data->memcpy_type == SliceCpy_t) {
-    size_t input_offset = Offset(tflite::micro::GetTensorShape(input), 0,
-                                 op_data->begin_y, op_data->begin_x, 0);
-    size_t num_bytes = (op_data->mf_params->input_height + 1) *
-                       op_data->mf_params->bytes_per_h_line;
-    memcpy((int8_t *)out_data, (int8_t *)in_data + input_offset, num_bytes);
-  } else if (op_data->memcpy_type == VpuCpy_t) {
-    op_data->memcpy_fn(op_data->mf_params, (int8_t *)out_data,
-                       (int8_t *)in_data, op_data->begin_y, op_data->begin_x,
-                       0);
-  } else if (op_data->memcpy_type == MemCpy_t) {
-    int num_in_bytes = input->dims->data[3];
-    int num_out_bytes = output->dims->data[3];
-    int num_of_pixels = output->dims->data[1] * output->dims->data[2];
-    for (int i = 0; i < num_of_pixels; i++) {
-      memcpy((int8_t *)out_data + (i * num_out_bytes),
-             (int8_t *)in_data + (i * num_in_bytes), num_out_bytes);
+  for (int i0 = op_data->begin[0]; i0 < op_data->end[0]; i0++) {
+    const int32_t in_idx0 = i0 * op_data->in_offsets[0];
+    const int32_t out_idx0 = (i0 - op_data->begin[0]) * op_data->out_offsets[0];
+    for (int i1 = op_data->begin[1]; i1 < op_data->end[1]; i1++) {
+      const int32_t in_idx1 = in_idx0 + i1 * op_data->in_offsets[1];
+      const int32_t out_idx1 =
+          out_idx0 + (i1 - op_data->begin[1]) * op_data->out_offsets[1];
+      for (int i2 = op_data->begin[2]; i2 < op_data->end[2]; i2++) {
+        const int32_t in_idx2 = in_idx1 + i2 * op_data->in_offsets[2];
+        const int32_t out_idx2 =
+            out_idx1 + (i2 - op_data->begin[2]) * op_data->out_offsets[2];
+        for (int i3 = op_data->begin[3]; i3 < op_data->end[3]; i3++) {
+          const int32_t in_idx3 = in_idx2 + i3 * op_data->in_offsets[3];
+          const int32_t out_idx3 =
+              out_idx2 + (i3 - op_data->begin[3]) * op_data->out_offsets[3];
+          if (op_data->is_vpu) {
+            vpu_memcpy((int8_t *)out_data + out_idx3,
+                       (int8_t *)in_data + in_idx3 + op_data->begin[4],
+                       op_data->out_offsets[3]);
+          } else {
+            memcpy((int8_t *)out_data + out_idx3,
+                   (int8_t *)in_data + in_idx3 + op_data->begin[4],
+                   op_data->out_offsets[3]);
+          }
+        }
+      }
     }
-  } else {
-    return kTfLiteError;
   }
-
   return kTfLiteOk;
 }
 

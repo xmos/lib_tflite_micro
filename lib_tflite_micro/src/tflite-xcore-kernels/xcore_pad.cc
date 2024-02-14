0bc1a7668e978f2c1cd2228e5660a0b3d093a9eb
Michael Poluektov <michael.poluektov@gmail.com>
2024-02-14 17:16:12 +0000

    works if no begin pad

--- lib_tflite_micro/src/tflite-xcore-kernels/xcore_pad.cc
+++ lib_tflite_micro/src/tflite-xcore-kernels/xcore_pad.cc
@@ -5,6 +5,7 @@
 #include "xcore_utils.h"
 
 extern "C" {
+#include "nn_op_utils.h"
 #include "lib_nn/api/nn_operator.h"
 }
 
@@ -14,51 +15,70 @@ namespace micro {
 namespace xcore {
 namespace pad {
 
-struct OpData {
-  nn_pad_plan_t *pad_plan;
-  uint32_t pad_value;
+struct PadOpData
+    : XCoreOpData { // Inherits the operator name field from XCoreOpData
+  int32_t begin[5];
+  int32_t end[5];
+  int32_t in_offsets[4];
+  int32_t out_offsets[4];
+  int32_t zero_point;
+  bool is_vpu;
 };
 
+void copy_vec(int32_t *dst, flexbuffers::Reference ref) {
+  auto vec = ref.AsVector();
+  for (int i = 0; i < vec.size(); i++) {
+    dst[i] = vec[i].AsInt32();
+  }
+}
 
-void* Init(TfLiteContext* context, const char* buffer, size_t length) {
-  auto op_data = construct_persistent_object<OpData>(context);
-
+void *Init(TfLiteContext *context, const char *buffer, size_t length) {
+  auto op_data = construct_persistent_object<PadOpData>(context);
+  op_data->name = "XC_Pad";
   auto parser = CustomOptionParser(buffer, length);
-  op_data->pad_plan = (nn_pad_plan_t*)parser.parseNamedCustomOption("pp").AsBlob().data();
-  auto pad_value = parser.parseNamedCustomOption("pv").AsUInt32();
-  op_data->pad_value = pad_value;
-  // op_data->n_threads = parser.parseNamedCustomOption("n_threads").AsUInt32();
+  copy_vec(op_data->begin, parser.parseNamedCustomOption("b"));
+  copy_vec(op_data->end, parser.parseNamedCustomOption("e"));
+  copy_vec(op_data->in_offsets, parser.parseNamedCustomOption("i"));
+  copy_vec(op_data->out_offsets, parser.parseNamedCustomOption("o"));
+  op_data->is_vpu = parser.parseNamedCustomOption("v").AsBool();
   return op_data;
 }
 
-TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
-  MicroContext* micro_context = GetMicroContext(context);
+TfLiteStatus Prepare(TfLiteContext *context, TfLiteNode *node) {
+  MicroContext *micro_context = GetMicroContext(context);
+  auto op_data = construct_persistent_object<PadOpData>(context);
+  TfLiteTensor *input = micro_context->AllocateTempInputTensor(node, 0);
+  TF_LITE_ENSURE(context, input != nullptr);
+  op_data->zero_point = input->params.zero_point;
+  micro_context->DeallocateTempTfLiteTensor(input);
   return kTfLiteOk;
 }
 
-TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
+TfLiteStatus Eval(TfLiteContext *context, TfLiteNode *node) {
   TFLITE_DCHECK(node->user_data != nullptr);
-  const OpData* data = static_cast<const OpData*>(node->user_data);
 
-  const TfLiteEvalTensor* input =
-      tflite::micro::GetEvalInput(context, node, /*index=*/0);
-
-  TfLiteEvalTensor* output =
-      tflite::micro::GetEvalOutput(context, node, /*index=*/0);
-
-  int8_t *output_p =
-      const_cast<int8_t *>(tflite::micro::GetTensorData<int8_t>(output));
-  int8_t *input_p =
-      const_cast<int8_t *>(tflite::micro::GetTensorData<int8_t>(input));
-
-  pad_run((char*)output_p,
-          (char*)input_p,
-          data->pad_plan, data->pad_value);
-
+  auto *op_data = static_cast<PadOpData *>(node->user_data);
+  // Get Input/Output Tensors
+  const TfLiteEvalTensor *input = tflite::micro::GetEvalInput(context, node, 0);
+  TfLiteEvalTensor *output = tflite::micro::GetEvalOutput(context, node, 0);
+  // Pointers to data in In/Out Tensors
+  const void *in_data = tflite::micro::GetTensorData<void>(input);
+  void *out_data = tflite::micro::GetTensorData<void>(output);
+  // TODO [michaelp]: Make this cleaner
+  // Get size of output buffer
+  size_t out_size;
+  TfLiteTypeSizeOf(output->type, &out_size);
+  for (int i = 0; i < output->dims->size; i++) {
+    out_size *= output->dims->data[i];
+  }
+  vpu_memset_32(out_data, op_data->zero_point, out_size / 4);
+  slice_memcpy((int8_t *)out_data, (int8_t *)in_data, op_data->out_offsets,
+               op_data->in_offsets, op_data->begin, op_data->end,
+               op_data->is_vpu ? memcpy_wrapper : vpu_memcpy);
   return kTfLiteOk;
 }
 
-}  // namespace pad
+} // namespace pad
 
 TFLMRegistration *Register_XC_pad() {
   static TFLMRegistration r = {pad::Init, nullptr, pad::Prepare, pad::Eval};
@@ -66,6 +86,6 @@ TFLMRegistration *Register_XC_pad() {
 }
 
 } // namespace xcore
-}  // namespace micro
-}  // namespace ops
-}  // namespace tflite
+} // namespace micro
+} // namespace ops
+} // namespace tflite

f23f0cba7b4cf2a8ba47c14147a97ace4f3681c8
panickal-xmos <deepakpanickal@xmos.com>
2024-03-04 11:32:13 +0000

    Add initial concept for blob approach

--- lib_tflite_micro/src/tflite-xcore-kernels/xcore_ops.cc
+++ lib_tflite_micro/src/tflite-xcore-kernels/xcore_ops.cc
@@ -30,6 +30,9 @@ void RegisterXCOps(MicroOpResolver *res) {
                       Register_XC_beta_transposeconvf32());
   resolver->AddCustom(XC_beta_fcf32_OpCode, Register_XC_beta_fcf32());
   resolver->AddCustom(XC_binaryi16_OpCode, Register_XC_binaryi16());
+
+  resolver->AddCustom(XC_blob_unaryi16_OpCode, Register_XC_blob_unaryi16());
+
   resolver->AddCustom(XC_unaryi16_OpCode, Register_XC_unaryi16());
   resolver->AddCustom(XC_conv2d_v2_OpCode, Register_XC_conv2d_v2());
   resolver->AddCustom(XC_maxpool2d_OpCode, Register_XC_maxpool2d());

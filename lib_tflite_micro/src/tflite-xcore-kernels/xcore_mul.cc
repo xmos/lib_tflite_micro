f0d15d29cad230033e0db23a115d1259f888dd17
panickal-xmos <deepakpanickal@xmos.com>
2025-04-03 21:11:39 +0100

    Multithread add and mul

--- lib_tflite_micro/src/tflite-xcore-kernels/xcore_mul.cc
+++ lib_tflite_micro/src/tflite-xcore-kernels/xcore_mul.cc
@@ -1,9 +1,12 @@
 // Copyright (c) 2023, XMOS Ltd, All rights reserved
 
+#include "../thread_call.h"
+#include "xcore_config.h"
 #include "xcore_custom_options.h"
 #include "xcore_utils.h"
 extern "C" {
 #include "lib_nn/api/nn_layers.h"
+#include "lib_nn/api/nn_operator.h"
 }
 
 namespace tflite_micro {
@@ -12,9 +15,28 @@ namespace micro {
 namespace xcore {
 namespace mul {
 
+struct MulShared {
+  int8_t *Y;
+  int8_t *X1;
+  int8_t *X2;
+  nn_mul_params_t *blob;
+};
+
+extern "C" {
+void mul_thread_worker(void *shared, void *start, void *end) {
+  int *s = static_cast<int *>(start);
+  int *e = static_cast<int *>(end);
+  auto sd = static_cast<MulShared *>(shared);
+  mul_elementwise(sd->X1 + *s, sd->X2 + *s, *e - *s, sd->blob, sd->Y + *s);
+}
+}
+
 // This is the struct that contains the data required by the operator
 struct MulOpData {
   nn_mul_params_t *mp_params;
+  int tc;
+  int s[XCORE_MAX_NUM_THREADS];
+  int e[XCORE_MAX_NUM_THREADS];
 };
 
 void *Init(TfLiteContext *context, const char *buffer, size_t length) {
@@ -28,6 +50,14 @@ void *Init(TfLiteContext *context, const char *buffer, size_t length) {
 
 // Does all the requests for scratches
 TfLiteStatus Prepare(TfLiteContext *context, TfLiteNode *node) {
+  auto *op_data = static_cast<MulOpData *>(node->user_data);
+  MicroContext *micro_context = GetMicroContext(context);
+  xc_context_config_t *xc_config = reinterpret_cast<xc_context_config_t *>(
+      micro_context->external_context());
+  const TfLiteEvalTensor *output =
+      tflite_micro::micro::GetEvalOutput(context, node, 0);
+  int output_size = tflite_micro::micro::GetTensorShape(output).FlatSize();
+  op_data->tc = calculateAlignedThreadSplit(xc_config->model_thread_count, output_size, op_data->s, op_data->e);
   return kTfLiteOk;
 }
 
@@ -47,8 +77,22 @@ TfLiteStatus Eval(TfLiteContext *context, TfLiteNode *node) {
   const int8_t *in1_data = tflite_micro::micro::GetTensorData<int8_t>(input1);
   const int8_t *in2_data = tflite_micro::micro::GetTensorData<int8_t>(input2);
 
-  int output_size = tflite_micro::micro::GetTensorShape(output).FlatSize();
-  mul_elementwise(in1_data, in2_data, output_size, op_data->mp_params, out_data);
+  MicroContext *micro_context = GetMicroContext(context);
+  xc_context_config_t *xc_config = reinterpret_cast<xc_context_config_t *>(
+      micro_context->external_context());
+  const int tc = op_data->tc;
+  MulShared shared_data;
+  shared_data.Y = out_data;
+  shared_data.X1 = const_cast<int8_t *>(in1_data);
+  shared_data.X2 = const_cast<int8_t *>(in2_data);
+  shared_data.blob = op_data->mp_params;
+  for (int t = 0; t < tc - 1; t++) {
+    thread_variable_setup((void *)&op_data->s[t], (void *)&op_data->e[t],
+                          xc_config->thread_info.thread_ids.id[t]);
+  }
+  thread_call((void *)&shared_data, &op_data->s[tc - 1], &op_data->e[tc - 1],
+              (thread_function_pointer_t)mul_thread_worker,
+              &xc_config->thread_info);
 
   return kTfLiteOk;
 }

4193b5dae59bd70ba388e376b761856a1325befb
xalbertoisorna <albertoisorna@xmos.com>
2026-09-23 09:52:15 +0100

    adding xcore build support

--- Makefile
+++ Makefile
@@ -8,6 +8,14 @@ build:
 	cmake -E chdir lib_tflite_micro ../version_check.sh
 	make -j8 -C build
 
+build_xs3:
+	cmake -B build_xs3 --toolchain=lib_tflite_micro/submodules/xmos_cmake_toolchain/xs3a.cmake
+	cmake --build build_xs3
+
+build_vx4:
+	cmake -B build_vx4 --toolchain=lib_tflite_micro/submodules/xmos_cmake_toolchain/vx4_xcc.cmake -DENABLE_SIZE_OPT=ON
+	cmake --build build_vx4
+
 test:
 	(cd host_cmd_line_interpreter && make test)
 	@echo ""
--- cmakefiles/deps.cmake
+++ cmakefiles/deps.cmake
@@ -1,8 +1,11 @@
 # Provides the lib_nn target and LIB_NN_ROOT_DIR for lib_nn/api includes.
-set(LIB_NN_REPOSITORY       "https://github.com/xmos/lib_nn.git")
-set(LIB_NN_TAG              "develop")
-
 include(FetchContent)
+
+set(LIB_NN_REPOSITORY       "git@github.com:xalbertoisorna/lib_nn.git") #TODO change to upstream
+set(LIB_NN_TAG              "fix/cmake")
+set(LIB_XUD_REPOSITORY      "https://github.com/xmos/lib_xud.git")
+set(LIB_XUD_TAG             "v2.4.0")
+
 FetchContent_Declare(
 	lib_nn
 	GIT_REPOSITORY          "${LIB_NN_REPOSITORY}"
@@ -12,3 +15,11 @@ FetchContent_Declare(
 FetchContent_MakeAvailable(lib_nn)
 FetchContent_GetProperties(lib_nn SOURCE_DIR LIB_NN_ROOT_DIR)
 target_include_directories(lib_nn INTERFACE "$<BUILD_INTERFACE:${LIB_NN_ROOT_DIR}>")
+
+FetchContent_Declare(
+	lib_xud
+	GIT_REPOSITORY "${LIB_XUD_REPOSITORY}"
+	GIT_TAG        "${LIB_XUD_TAG}"
+)
+FetchContent_Populate(lib_xud)
+set(LIB_XUD_ROOT_DIR "${lib_xud_SOURCE_DIR}")
